028d80991f8767364569ff04a54a015e33cc63b4
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-01-12 00:06:43 +0100

    Add files via upload
    Committing Main class.

---
+++ src/com/mynumbers/Main.java
@@ -0,0 +1,27 @@
+/**
+ *  Java program to use java.lang.Math class.
+ */
+package com.mynumbers;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    // Main method to run java program.
+    public static void main(String[] args) {
+
+        // Finding minimum of two numbers 6 and 12.
+        int minNumber = Math.min(6, 12);
+
+        // Printing minimum number to console.
+        System.out.println(minNumber);
+
+        // Finding maximum of two numbers.
+        int maxNumber = Math.max(7, 12);
+
+        // Printing maximum number to console.
+        System.out.println(maxNumber);
+
+    }
+}
